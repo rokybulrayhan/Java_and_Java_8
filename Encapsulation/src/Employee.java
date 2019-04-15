b884b7a526fef5286d85043d5a5a59973907aef9
Rokybul Rayhan Chowdhury <48471194+rokybulrayhan@users.noreply.github.com>
2019-04-15 11:43:38 +0600

    Add files via upload

---
+++ Encapsulation/src/Employee.java
@@ -0,0 +1,13 @@
+
+
+public class Employee{
+	public static String name="Rayhan";
+        private double salary;
+	protected int age;
+        Employee (int age,double salary ){
+	
+		
+		
+	}
+	
+}
